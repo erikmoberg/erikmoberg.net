122592f0f5c25e4e2d4083b9ac69349bd84f81f7
erikmoberg <test@example.com>
2019-02-21 22:27:00 +0100

    Ten Tiny Levels

---
+++ ten-tiny-levels.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Ten Tiny Levels by Regal Raccoon Games</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="/ttl-assets/style.css?_=8" />
+		<link rel="shortcut icon" href="/favicon.ico" type="image/vnd.microsoft.icon" />
+	  <link rel="icon" type="image/png" href="/ttl-assets/favicon-192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="/ttl-assets/favicon-160.png" sizes="160x160">
+		<link rel="icon" type="image/png" href="/ttl-assets/favicon-96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="/ttl-assets/favicon-16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="/ttl-assets/favicon-32.png" sizes="32x32">
+		<meta name="robots" content="index, follow" />
+		<meta name="googlebot" content="index, follow" />
+		<meta name="description" content="Ten Tiny Levels by Regal Raccoon Games" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<div id="menucontainer">
+			<img class="logo" alt="Regal Raccoon Games Logo" src="/ttl-assets/logo2.png">
+		</div>
+		<div id="containercontainer">
+			<div id="container">
+				<div id="content">
+					<div class="page-section">
+						<h2>Fast-paced platform arcade action for one or two players!</h2>
+						<p>
+							Eliminate all enemies and grab all boxes before the timer runs out. Work together with a friend in cooperative mode or challenge your personal nemesis in versus mode on a single device.
+						</p>
+						<div style="text-align: center; margin-top: -5px;">
+						<a href="https://play.google.com/store/apps/details?id=com.regalraccoongames.tentinylevels">
+							<img class="google-play-badge" src="/ttl-assets/google-play-badge.png">
+						</a>
+					</div>
+						<h2>Main features:</h2>
+						<ul>
+							<li>Single-finger controls without on-screen buttons</li>
+							<li>Portrait orientation</li>
+							<li>Same device co-op and versus multiplayer</li>
+							<li>7 rewarding and easy to use weapons</li>
+							<li>No ads or in-app purchases</li>
+							<li>Colorful pixelated retro graphics (because I can't draw)</li>
+						</ul>
+						<div style="text-align: center; margin-top: 20px;">
+							<iframe width="560" height="315" style="max-width: 100%;" src="https://www.youtube.com/embed/YBwsQeFVNVE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="screenshots-list">
+						<div><img src="/ttl-assets/screenshot-01.png"></div>
+						<div><img src="/ttl-assets/screenshot-02.png"></div>
+						<div><img src="/ttl-assets/screenshot-03.png"></div>
+						<div><img src="/ttl-assets/screenshot-04.png"></div>
+						<div><img src="/ttl-assets/screenshot-05.png"></div>
+						<div><img src="/ttl-assets/screenshot-06.png"></div>
+					</div>
+					<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+		</div>
+		<div id="bottom" style="text-align: center;">
+			<br />
+			<img alt="Regal Raccoon Games Logo" class="logo" src="/ttl-assets/logo2.png">
+			<br />
+			<span class="logo"><?php echo date("Y"); ?></span>
+		</div>
+	</body>
+</html>
